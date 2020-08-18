8e2d74e5191feb2048212763933adec07a5e5060
thelittlecitizen12 <thelittlecitizen12@gmail.com>
2020-08-18 19:07:05 +0300

    Build body of the project

--- MenuBuilder/MenuBuilder/MainMenu.cs
+++ MenuBuilder/MenuBuilder/MainMenu.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace MenuBuilder
 {
-    class MainMenu
+    public class MainMenu :IMenu
     {
+        
+
+        public void Run<TKey,TValue>(Dictionary<TKey, TValue> menuDict)
+        {
+            bool showMenu = true;
+            
+
+
+                Console.WriteLine("Choose an option from the menu: ");
+                foreach(KeyValuePair<TKey,TValue> item in menuDict)
+                {
+                    Console.WriteLine("{0} {1}", item.Key, item.Value);
+                }
+                Console.ReadLine();
+            }
+            
+            
+        }
+
+       
+        public void AddOption<TKey, TValue>(ref TKey value,ref TValue key)
+        {
+            Dictionary<TKey, TValue> menuDict = new Dictionary<TKey, TValue>();
+            menuDict.Add(value, key);
+        }
+
+        
     }
 }
